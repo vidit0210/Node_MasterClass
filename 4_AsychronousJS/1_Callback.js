fab20c1d7ecb259485b9e903ac9408703b2498b2
Vidit Shah <vidit0210@gmail.com>
2019-05-29 18:48:11 +0530

    Traverse

--- 4_AsychronousJS/1_Callback.js
+++ 4_AsychronousJS/1_Callback.js
@@ -7,4 +7,20 @@ let y = function(callback) {
 };
 y(x);
 
-let calc = function(num1, num2, calcType) {};
+let add = function(a, b) {
+  return a + b;
+};
+
+let mul = function(a, b) {
+  return a * b;
+};
+let calc = function(num1, num2, callback) {
+  return callback(num1, num2);
+};
+
+console.log(calc(3, 5, add));
+console.log(
+  calc(4, 1, (a, b) => {
+    return a - b;
+  })
+);
