c31a287459a291fd02e637efee61fc9102ce1994
Vidit Shah <vidit0210@gmail.com>
2019-05-29 15:48:57 +0530

    pug

---
+++ 3_Adavnce_Express/app.js
@@ -0,0 +1,109 @@
+const config = require("config");
+const express = require("express");
+const Joi = require("joi"); //Returns a Class
+const app = express();
+
+//Setting the View Engine
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+//Envirnonmental Variables
+const _PORT = process.env.PORT || 3000;
+
+console.log(config.get("name"));
+//Middleware
+app.use(express.json());
+app.use(express.static("./public"));
+//Defining Courses Array
+
+const courses = [
+  { id: 1, course: "Python" },
+  { id: 2, course: "JavaScript" },
+  { id: 3, course: "Ruby" }
+];
+app.get("/", (req, res) => {
+  res.render("index", { title: "Express App", heading: "Welcome" });
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+//parameters uses :id routes
+app.get("/api/courses/:id", (req, res) => {
+  let index = parseInt(req.params.id);
+
+  const result = courses.find(c => {
+    return c.id === index;
+  });
+  if (!result) res.status(404).send("Page Not Found!");
+  res.send(result);
+});
+
+//Post Method
+app.post("/api/courses", (req, res) => {
+  const schema = {
+    id: Joi.number().required(),
+    course: Joi.string()
+      .min(3)
+      .required()
+  };
+
+  const { error, value } = Joi.validate(req.body, schema);
+
+  if (error) {
+    return res.status(404).send(`Bad Reequest ${error}`);
+  }
+  const coureToBeAdded = {
+    id: courses.length + 1,
+    course: req.body.course
+  };
+
+  courses.push(coureToBeAdded);
+  res.send(`Course added to Id ${coureToBeAdded.id}`);
+});
+
+//Update  The Course  -- Put method
+
+app.put("/api/courses/:id", (req, res) => {
+  let index = parseInt(req.params.id);
+  let result = courses.findIndex(course => {
+    return course.id === index;
+  });
+  console.log(result);
+  if (result === -1) return res.status(404).send("No Such Course");
+
+  let { error, value } = validate(req);
+  if (error) return res.status(400).send("Bad Request" + error);
+  courses[result].course = req.body.course;
+  res.send(
+    "Updated course at " + courses[result].id + " is " + courses[result].course
+  );
+});
+
+app.delete("/api/courses/:id", (req, res) => {
+  let index = parseInt(req.body.id);
+  let result = courses.findIndex(course => {
+    return course.id === index;
+  });
+  console.log(result);
+  if (result < 0) {
+    return res.status(404).send("No such course in the Database AVAIALABLE");
+  }
+  courses.splice(result, 1);
+  res.send(courses);
+});
+
+app.listen(_PORT, () => {
+  console.log(`Listening to Port ${_PORT}`);
+});
+
+function validate(request) {
+  const schema = {
+    id: Joi.number().required(),
+    course: Joi.string()
+      .min(3)
+      .required()
+  };
+  return Joi.validate(request.body, schema);
+}
--- 99_Vidly/app.js
+++ 99_Vidly/app.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const Joi = require("joi");
+const express = require("express"); //Function
+const Joi = require("joi"); // Class
 const app = express();
 
 const _PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json()); //To accept JSON Transaction
 
 __movies = [
   { id: 1, name: "ABC", genre: "Action" },
